a7b8523c67e59e4b68bb7eb3266fe131e02cee70
mxblsdl <maxblasdel@gmail.com>
2024-10-20 21:19:23 +0000

    add file summary

--- pycounter/main.py
+++ pycounter/main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from typing_extensions import Annotated
 from pycounter.models import Stats
+from pycounter.table import create_table
 
 # A super cool CLI
 app = typer.Typer(
@@ -14,24 +15,43 @@ app = typer.Typer(
 @app.command()
 def count(
     path: Annotated[str, typer.Argument()] = "./",
-    ext: Annotated[str, typer.Option("--ext", "-e")] = ".py",
+    ext: Annotated[str, typer.Option("--ext", "-e")] = None,
 ):
     stats = Stats()
 
     files = find_files(path, ext)
 
+    if not ext:
+        file_summary = create_file_summary(files)
+        create_table(file_summary, title="File type summary")
+        raise typer.Exit(0)
+
     for file in files:
+        # TODO different process func for different extensions
         process_file(file, stats)
 
-    typer.echo(stats)
+    stats.calc_lines()
+    stats.calc_files()
+
+    create_table(
+        stats.dict("files"),
+        title="Files Stats",
+        caption="A file count summary",
+    )
+    create_table(
+        stats.dict("lines"),
+        title="Lines Stats",
+    )
 
 
-def find_files(path: str, ext: str):
+def find_files(path: str, ext: str | None):
     files = Path(path).rglob("*")
     # Filter for files and ignore any .git files or folders
     files = [f for f in files if f.is_file() and ".git" not in f.parts]
     # TODO accept list of ext
-    return [f for f in files if ext == f.suffix]
+    if ext:
+        return [f for f in files if ext == f.suffix]
+    return files
 
 
 def process_file(file_path: str, stats: Stats):
@@ -41,42 +61,58 @@ def process_file(file_path: str, stats: Stats):
             DOCSTRING_FLAG = False
 
             if Path(file_path).stat().st_size == 0:
-                typer.echo(f"empty file {file.name}")
+                # typer.echo(f"empty file {file.name}")
                 stats.add("empty_files")
 
             for line in file:
-                if line == "\n" and not DOCSTRING_FLAG:
+                if line.strip() == "" and not DOCSTRING_FLAG:
                     stats.add("blank_lines")
                     continue
 
                 if '"""' in line.split():
                     if line.split()[0] == '"""':
-                        stats.add("doc_strings")
+                        stats.add("docstring_lines")
                         DOCSTRING_FLAG = not DOCSTRING_FLAG
                         continue
 
                 if DOCSTRING_FLAG:
-                    stats.add("doc_strings")
+                    stats.add("docstring_lines")
                     continue
 
                 if line.startswith("import"):
-                    stats.add("imports")
+                    stats.add("import_lines")
                     continue
 
                 if line.startswith("from") and "import" in line.split():
-                    stats.add("imports")
+                    stats.add("import_lines")
                     continue
 
                 if line.strip()[0] == "#":
-                    stats.add("comments")
+                    stats.add("comment_lines")
                     continue
 
-                stats.lines += 1
+                stats.total_lines += 1
 
     except FileNotFoundError:
         typer.echo(f"File {file_path} not found")
 
 
-# if __name__ == "__main__":
-# typer.run(app())
-#     typer.run(count)
+def create_file_summary(files: list[Path]) -> dict:
+    files_hash = dict()
+    for file in files:
+        if file.suffix == "":
+            key = file.name
+        else:
+            key = file.suffix
+        if key not in files_hash.keys():
+            files_hash[key] = 0
+        files_hash[key] += 1
+    files_hash = dict(
+        sorted(files_hash.items(), key=lambda item: item[1], reverse=True)
+    )
+    return {k: str(v) for k, v in files_hash.items()}
+
+
+if __name__ == "__main__":
+    # typer.run(app())
+    typer.run(count)
