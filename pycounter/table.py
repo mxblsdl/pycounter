dc3a8f277800c7b5b6d8d4f4e0efcac185f44583
mxblsdl <maxblasdel@gmail.com>
2024-10-20 21:18:54 +0000

    add table and console modules

---
+++ pycounter/table.py
@@ -0,0 +1,28 @@
+from rich.table import Table
+from rich import box
+from pycounter.console import console
+
+
+def create_table(stats: dict, title: str, **kwargs) -> None:
+    if console.is_dumb_terminal:
+        measure_style = None
+        value_style = None
+    else:
+        measure_style = "cyan"
+        value_style = "green"
+
+    table = Table(title=title, box=box.ASCII, **kwargs)
+
+    table.add_column("Measure", justify="right", style=measure_style, no_wrap=True)
+    table.add_column("Value", style=value_style)
+
+    for k, v in stats.items():
+        if k == "net_lines":
+            table.add_section()
+        if k == "net_files":
+            table.add_section()
+        table.add_row(k, v)
+
+    console.print(table)
+    console.line(1)
+    # console.save_svg(path="test.svg")
